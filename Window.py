8ba2a8618c80372ba8b576b28bd4a513085704b6
jan438 <jan438@users.noreply.github.com>
2024-01-18 14:58:54 +0100

    Print castling added

--- Window.py
+++ Window.py
@@ -268,6 +268,7 @@ class King(ChessPiece):
 
                 elif piece.grid_y == self.grid_y and piece.grid_x < self.grid_x and (piece.id[5:9] != "Rook" or self.id[:5] != piece.id[:5]):
                     no_piece_left = False
+            print("Castling:",self.grid_x,self.grid_y,self.id)
             if no_piece_right and no_piece_left:
                 return [(self.grid_x-2, self.grid_y),(self.grid_x+2, self.grid_y)]
             if no_piece_right:
