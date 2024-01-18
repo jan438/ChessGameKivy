1299e5aecf7865cd2320315e223837295622a2fc
jan438 <jan438@users.noreply.github.com>
2024-01-18 13:46:17 +0100

    Removed emty lines

--- Window.py
+++ Window.py
@@ -310,13 +310,10 @@ class King(ChessPiece):
 
                 elif piece.grid_y == self.grid_y and piece.grid_x < self.grid_x and (piece.id[5:9] != "Rook" or self.id[:5] != piece.id[:5]):
                     no_piece_left = False
-
             if no_piece_right and no_piece_left:
                 return [(self.grid_x-2, self.grid_y),(self.grid_x+2, self.grid_y)]
-
             if no_piece_right:
                 return [(self.grid_x+2, self.grid_y)]
-
             if no_piece_left:
                 return [(self.grid_x-2, self.grid_y)]
         return []
