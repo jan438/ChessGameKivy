055669aec48ba6a5b96bc1c8b88698431d4a04e1
jan438 <jan438@users.noreply.github.com>
2024-01-20 13:47:01 +0100

    Print opties expanded

--- Window.py
+++ Window.py
@@ -348,11 +348,14 @@ class ChessBoard(RelativeLayout):
             elif ChessBoard.piece_pressed and ChessBoard.id_piece_[5:] == "King" and (grid_x, grid_y) in ChessBoard.available_moves["castling"] and child.id[:5] == ChessBoard.id_piece_[:5] and child.id[5:-2] == "Rook" and child.First_use:
                 print("Castling uitvoeren",child.id,child.grid_x,self.children[ChessBoard.piece_index].id,"Available:",ChessBoard.available_moves["castling"])
                 if child.grid_x == grid_x + 1:
+                    print("Optie1:",grid_x)
                     anim = Animation(grid_x=grid_x-1, grid_y=grid_y, t='in_out_expo', duration=0.5)
                     anim.start(self.children[id])
                 elif child.grid_x == grid_x - 1:
+                    print("Optie2:",grid_x)
                     anim = Animation(grid_x=grid_x+1, grid_y=grid_y, t='in_out_expo', duration=0.5)
                     anim.start(self.children[id])
+                print("Optie3:",grid_x,child.id)
                 anim = Animation(grid_x=grid_x, grid_y=grid_y, t='in_out_expo', duration=0.5)
                 anim.start(self.children[ChessBoard.piece_index])
                 ChessBoard.piece_pressed = False
