6c856d16c25ac42c55e87af0590b7572f7a70a48
jan438 <jan438@users.noreply.github.com>
2024-12-29 14:57:22 +0100

    testpgn added

--- Window.py
+++ Window.py
@@ -542,5 +542,6 @@ if __name__ == '__main__':
     node = node.add_variation(chess.Move.from_uci("e7e5"))
     node.comment = "Comment"
     print(game)
-    print("PGN")
+    pgn = open("testpgn.pgn")
+    testgame = chess.pgn.read_game(pgn)
     ChessApp().run()
