55ba173b8303f87e0dc1aba6cd011dcd2f6dafcf
jan438 <jan438@users.noreply.github.com>
2025-01-02 15:54:46 +0100

    print extended

--- Window.py
+++ Window.py
@@ -388,7 +388,7 @@ Rg3 54. Qd8+ Ke6 55. Ra6 Kf5 56. Qf6+ Ke4 57. Ra4+ Kd3 58. Qd4+ Ke2 59. Ra2+ Kf1
                 return child
                 
     def listpgn_moves(self):
-        print("List pgn_moves")
+        print("List pgn_moves", self.pgn_index, len(self.pgn_moves))
                 
     def trace(self,id,nr):
         piece = self.findpiece(id)
