eb716a08c97f7627b48e23cbd4cc90d78677b8f6
jan438 <jan438@users.noreply.github.com>
2025-01-14 10:10:31 +0100

    anim replaced

--- Window.py
+++ Window.py
@@ -382,11 +382,11 @@ class ChessBoard(RelativeLayout):
             child = self.children[cindex]
             self.remove_widget(child)
         pindex = self.pieceindex_at_board(xfrom, yfrom)
-        anim = Animation(grid_x = xto, grid_y = yto, t='in_out_expo', duration=0.5)
         if pindex > -1:
             child = self.children[pindex]
             id = child.id
             child.First_use = False
+            anim = Animation(grid_x = xto, grid_y = yto, t='in_out_expo', duration=0.5)
             anim.start(child)
             if id == "WhiteKing" and xto == 6 and yto == 0:
                 piece = self.findpiece("WhiteRook_1")
