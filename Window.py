1bfddb56559f5c43850ad8547d8ec53b0ef904de
jan438 <jan438@users.noreply.github.com>
2025-01-14 10:33:13 +0100

    conditiie added

--- Window.py
+++ Window.py
@@ -387,7 +387,7 @@ class ChessBoard(RelativeLayout):
             id = child.id
             child.First_use = False
             if child.id[5:9] == "Pawn":
-                print(child.id)
+                print(child.id[:5], xfrom, yfrom, xto, yto)
             anim = Animation(grid_x = xto, grid_y = yto, t='in_out_expo', duration=0.5)
             anim.start(child)
             if id == "WhiteKing" and xto == 6 and yto == 0:
