e70b5a1905fb87dd0676f393d5b99f4e6952d9bb
jan438 <jan438@users.noreply.github.com>
2025-01-16 11:00:11 +0100

    opgngame ine board

--- Window.py
+++ Window.py
@@ -576,6 +576,7 @@ class ChessBoard(RelativeLayout):
             self.opgngame.headers["Black"] = "Garry Kasparov"
             current_date = datetime.today().strftime('%Y-%m-%d')
             self.opgngame.headers["Date"] = "1982.05.??"
+            pgnboard = self.opgngame.board()
             self.pgn_moves = []
             pgn = open("PGN/begin.txt", 'r')
             for line in pgn:
@@ -594,6 +595,8 @@ class ChessBoard(RelativeLayout):
             self.pgn_moves = []
             for move in self.opgngame.mainline_moves():
                 self.pgn_moves.append(move)
+                pgnboard.push(move)
+            print(pgnboard)
         elif l == 'n':
             if self.pgn_index > -1 and self.pgn_index < len(self.pgn_moves):
                 self.animate_pgn_move(self.pgn_index, self.pgn_moves[self.pgn_index])
