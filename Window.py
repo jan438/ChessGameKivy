a458ae4626059ec4ab53c8dc33266c2d82f45369
jan438 <jan438@users.noreply.github.com>
2025-02-23 11:15:32 +0100

    safe places functie added

--- Window.py
+++ Window.py
@@ -616,7 +616,7 @@ class ChessBoard(RelativeLayout):
             if yto == 7:
                 eplcs = [[3,7],[2,7],[1,7]]
                 splcs = [[4,7],[3,7],[2,7],[1,7]]
-                return (self.empty_places(eplcs))
+                return (self.safe_places(splcs, 'b') and self.empty_places(eplcs))
             if yto == 0:
                 eplcs = [[3,0],[2,0],[1,0]]
                 splcs = [[4,0],[3,0],[2,0],[1,0]]
@@ -630,8 +630,7 @@ class ChessBoard(RelativeLayout):
                 eplcs = [[5,0],[6,0]]
                 splcs = [[4,0],[5,0],[6,0]]
                 return (self.empty_places(eplcs))
-        #return False
-        return True
+        return False
 
     def valid_knight(self, xfrom, yfrom, xto, yto):
         deltax = abs(xfrom - xto)
@@ -697,7 +696,11 @@ class ChessBoard(RelativeLayout):
             index = y * 8 + x  
             if self.pgnboard.piece_at(index) != None:
                 return False
-        return True             
+        return True
+        
+    def safe_places(self, places, color):
+        print("safe_places")
+        return True        
 
     def make_pgn_move(self, keyboard, keycode, text, modifiers):
         l = keycode[1]
