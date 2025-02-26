4f9760dff194c558cab266653f592e3f0dee164f
jan438 <jan438@users.noreply.github.com>
2025-02-26 11:11:55 +0100

    print extend

--- Window.py
+++ Window.py
@@ -604,7 +604,7 @@ class ChessBoard(RelativeLayout):
     def valid_pawn(self, xfrom, yfrom, xto, yto):
         deltax = xfrom - xto
         deltay = yfrom - yto
-        print("Valid pawn from", xfrom, yfrom, "to", xto, yto)
+        print("Valid pawn from", xfrom, yfrom, "to", xto, yto, "wep", wep, "bep", bep)
         if deltax == 0 and deltay == -2 and yfrom == 1 and self.pgnboard.piece_at((yfrom + 1) * 8 + xto) == None and self.pgnboard.piece_at((yfrom + 2) * 8 + xto) == None :
                 return True
         elif deltax == 0 and deltay == -1 and self.pgnboard.piece_at((yfrom + 1) * 8 + xto) == None:
