02585fb4d7c24d220478ba74bc1dd4761db02635
jan438 <jan438@users.noreply.github.com>
2025-02-26 10:02:11 +0100

    elif added

--- Window.py
+++ Window.py
@@ -606,21 +606,19 @@ class ChessBoard(RelativeLayout):
         deltay = yfrom - yto
         if deltax == 0 and deltay == -2 and yfrom == 1 and self.pgnboard.piece_at((yfrom + 1) * 8 + xto) == None and self.pgnboard.piece_at((yfrom + 2) * 8 + xto) == None :
                 return True
-        if deltax == 0 and deltay == -1 and self.pgnboard.piece_at((yfrom + 1) * 8 + xto) == None:
+        elif deltax == 0 and deltay == -1 and self.pgnboard.piece_at((yfrom + 1) * 8 + xto) == None:
                 return True
-        if deltax == 0 and deltay == 2 and yfrom == 6 and self.pgnboard.piece_at((yfrom - 1) * 8 + xto) == None and self.pgnboard.piece_at((yfrom - 2) * 8 + xto) == None :
+        elif deltax == 0 and deltay == 2 and yfrom == 6 and self.pgnboard.piece_at((yfrom - 1) * 8 + xto) == None and self.pgnboard.piece_at((yfrom - 2) * 8 + xto) == None :
                 return True
-        if deltax == 0 and deltay == 1 and self.pgnboard.piece_at((yfrom - 1) * 8 + xto) == None:
+        elif deltax == 0 and deltay == 1 and self.pgnboard.piece_at((yfrom - 1) * 8 + xto) == None:
                 return True
-        if abs(deltax == 1) and deltay == -1 and self.pgnboard.piece_at((yfrom + 1) * 8 + xto) != None:
+        elif abs(deltax == 1) and deltay == -1 and self.pgnboard.piece_at((yfrom + 1) * 8 + xto) != None:
                 return True
-        if abs(deltax == 1) and deltay == 1 and self.pgnboard.piece_at((yfrom - 1) * 8 + xto) != None:
+        elif abs(deltax == 1) and deltay == 1 and self.pgnboard.piece_at((yfrom - 1) * 8 + xto) != None:
                 return True 
-        if abs(deltax == 1) and deltay == -1 and bep[xto]:
-            if self.pgnboard.piece_at((yfrom + 1) * 8 + xto) == None:
+        elif abs(deltax == 1) and deltay == -1 and bep[xto] and self.pgnboard.piece_at((yfrom + 1) * 8 + xto) == None:
                 return True
-        if abs(deltax == 1) and deltay == 1 and wep[xto]:
-            if self.pgnboard.piece_at((yfrom - 1) * 8 + xto) == None:
+        elif abs(deltax == 1) and deltay == 1 and wep[xto] and self.pgnboard.piece_at((yfrom - 1) * 8 + xto) == None:
                 return True        
         return False
         
