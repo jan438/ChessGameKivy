2d3c76f9ec1457f53b6f67093f8fff9fdf241e76
jan438 <jan438@users.noreply.github.com>
2025-02-27 15:41:17 +0100

    prints removed

--- Window.py
+++ Window.py
@@ -604,26 +604,17 @@ class ChessBoard(RelativeLayout):
     def valid_pawn(self, xfrom, yfrom, xto, yto):
         deltax = xfrom - xto
         deltay = yfrom - yto
-        print("Valid pawn from", xfrom, yfrom, "to", xto, yto, "deltax", deltax, "deltay", deltay)
-        print("wep", wep, "x", wep[xto])
-        print("bep", bep, "x", bep[xto])
-        if deltax == 0 and deltay == -2 and yfrom == 1 and self.pgnboard.piece_at((yfrom + 1) * 8 + xto) == None and self.pgnboard.piece_at((yfrom + 2) * 8 + xto) == None :
-                print("2 places w")     
+        if deltax == 0 and deltay == -2 and yfrom == 1 and self.pgnboard.piece_at((yfrom + 1) * 8 + xto) == None and self.pgnboard.piece_at((yfrom + 2) * 8 + xto) == None :    
                 return True
-        elif deltax == 0 and deltay == -1 and self.pgnboard.piece_at((yfrom + 1) * 8 + xto) == None:
-                print("1 places w")     
+        elif deltax == 0 and deltay == -1 and self.pgnboard.piece_at((yfrom + 1) * 8 + xto) == None:   
                 return True
-        elif deltax == 0 and deltay == 2 and yfrom == 6 and self.pgnboard.piece_at((yfrom - 1) * 8 + xto) == None and self.pgnboard.piece_at((yfrom - 2) * 8 + xto) == None :
-                print("2 places b")     
+        elif deltax == 0 and deltay == 2 and yfrom == 6 and self.pgnboard.piece_at((yfrom - 1) * 8 + xto) == None and self.pgnboard.piece_at((yfrom - 2) * 8 + xto) == None :   
                 return True
-        elif deltax == 0 and deltay == 1 and self.pgnboard.piece_at((yfrom - 1) * 8 + xto) == None:
-                print("1 places b")     
+        elif deltax == 0 and deltay == 1 and self.pgnboard.piece_at((yfrom - 1) * 8 + xto) == None: 
                 return True
-        elif abs(deltax) == 1 and deltay == -1 and self.pgnboard.piece_at(yto * 8 + xto) != None:
-                print("capture by w")     
+        elif abs(deltax) == 1 and deltay == -1 and self.pgnboard.piece_at(yto * 8 + xto) != None:  
                 return True
-        elif abs(deltax) == 1 and deltay == 1 and self.pgnboard.piece_at(yto * 8 + xto) != None:
-                print("capture by b")     
+        elif abs(deltax) == 1 and deltay == 1 and self.pgnboard.piece_at(yto * 8 + xto) != None: 
                 return True 
         elif yfrom == 4 and bep[xto] and deltay == -1 and abs(deltax) == 1 and self.pgnboard.piece_at(yto * 8 + xto) == None:
                 return True
